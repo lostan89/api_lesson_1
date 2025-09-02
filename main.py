b4ac95b60e2a1f167a9a1c030829cd9ab4cf6801
lostan89 <skeeperenator@gmail.com>
2025-09-02 21:01:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import requests
 
 def get_weather(city):
     url = "https://wttr.in/{}".format(city)
-    payload = {"MqnTqu": "", "lang": "ru"}
+    payload = {"MqnT": "", "lang": "ru"}
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    print(response.text)
+    return response.text
 
 
 def main():
-    get_weather("Лондон")
-    get_weather("Шереметьево")
-    get_weather("Череповец")
+    locations = ["Лондон", "SVO", "Череповец"]
+    for location in locations:
+        print(get_weather(location))
 
 
 if __name__ == "__main__":
